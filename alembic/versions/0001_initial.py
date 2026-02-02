60820b1e64765560834c422d48bb72aa919dd25f
Vedanth <veduupadhyaya@gmail.com>
2026-02-02 17:29:24 +0530

    fix: migration

--- alembic/versions/0001_initial.py
+++ alembic/versions/0001_initial.py
@@ -1,13 +1,13 @@
 """initial
 
-Revision ID: 0001_initial
+Revision ID: 0001
 Revises: 
 Create Date: 2026-01-09 00:00:00
 """
 from alembic import op
 import sqlalchemy as sa
 
-revision = '0001_initial'
+revision = '0001'
 down_revision = None
 branch_labels = None
 depends_on = None
